73b91b3812456541e37f4ca56f8c145a4b133481
ziming <ziye@ucsc.edu>
2018-11-28 23:31:41 -0800

    Merging

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$acc896c459eb6b1c$84e391931a5c0f0fc7edbcfaf90d42b8a4dc550b"
-=======
 password="pbkdf2(1000,20,sha512)$acb65b2b5ba442f2$15edad7ed8962c1398a6705a1903ce0975d06da5"
->>>>>>> 0b6d84bf32a5717ab61756c7c55173fca99f5c1f
